eff6e16fa5204eea803cfa75d9b022955f460bd5
ZeroDi <zerodi0000@gmail.com>
2026-08-18 23:52:14 +0900

    build: export generated calculator registry runtime

--- dist/index.js
+++ dist/index.js
@@ -5,4 +5,6 @@ export { defineZdpErrorContract } from './error/index.js';
 export { defineI18nMessageContract } from './i18n-contract/index.js';
 export { defineGlossaryTermContract } from './glossary-contract/index.js';
 export { CALCULATOR_CONTRACT_VERSION, CALCULATOR_ENGINE_VERSION, CALCULATOR_MAX_DECIMAL_PLACES, CALCULATOR_MAX_INPUT_DIGITS, CALCULATOR_ROUNDING_MODE, COMPOUNDING_FREQUENCIES, COMPOUND_INTEREST_MAX_POWER_DIGITS, COMPOUND_INTEREST_MAX_YEARS, DATE_BOUNDARY_MODES, DATA_RATE_UNITS, DATA_SIZE_UNITS, DISCOUNT_MODES, FUEL_ECONOMY_UNITS, OVERNIGHT_MODES, TRIP_MODES, calculateAge, calculateBreakEvenPoint, calculateDataTransferTime, calculateDateDifference, calculateDiscount, calculateCompoundInterest, calculateFuelCost, calculateKioskRoi, calculateLockerRevenue, calculateMarginMarkup, calculatePercentageChange, calculateSecurityCostBreakEven, calculateStudycafeBreakEven, calculateStudycafeSeatOccupancy, calculateStudyRoomScheduleRevenue, calculateUnattendedLaborSavings, calculateWorkHours } from './calculator-engine/index.js';
+export { CALCULATORS, CALCULATOR_IDS, CALCULATOR_REQUIRED_ENGINE_VERSION } from './calculator-engine/catalog.generated.js';
+export { calculateById } from './calculator-engine/registry.generated.js';
 //# sourceMappingURL=index.js.map
